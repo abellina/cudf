9515394aa89ff623c0c4a214c8ed5101f1d88521
vuule <vmilovanovic@nvidia.com>
2021-04-26 17:08:27 -0700

    busy_wait

--- cpp/src/io/comp/unsnap.cu
+++ cpp/src/io/comp/unsnap.cu
@@ -29,6 +29,16 @@ constexpr int32_t batch_count   = (1 << 2);
 constexpr int32_t prefetch_size = (1 << 9);  // 512B, in 32B chunks
 constexpr bool log_cyclecount   = false;
 
+void __device__ busy_wait(size_t cycles)
+{
+  clock_t start = clock();
+  for (;;) {
+    clock_t const now    = clock();
+    clock_t const passed = now > start ? now - start : now + (0xffffffff - start);
+    if (passed >= cycles) return;
+  }
+}
+
 /**
  * @brief Describes a single LZ77 symbol (single entry in batch)
  */
@@ -306,7 +316,7 @@ __device__ void snappy_decode_symbols(unsnap_state_s *s, uint32_t t)
       s->q.prefetch_rdpos = cur;
       uint32_t lcnt       = 0;
 #pragma unroll(1)  // We don't want unrolling here
-      while (s->q.prefetch_wrpos < min(cur + 5 * batch_size, end)) { busy_wait(10); }
+      while (s->q.prefetch_wrpos < min(cur + 5 * batch_size, end)) { busy_wait(50); }
       b = &s->q.batch[batch * batch_size];
     }
     // Process small symbols in parallel: for data that does not get good compression,
@@ -484,7 +494,7 @@ __device__ void snappy_decode_symbols(unsnap_state_s *s, uint32_t t)
     batch_len = shuffle(batch_len);
     uint32_t lcnt = 0;
     if (t == 0) {
-      while (s->q.batch_len[batch] != 0) { busy_wait(20); }
+      while (s->q.batch_len[batch] != 0) { busy_wait(100); }
     }
     if (batch_len != batch_size) { break; }
   }
@@ -517,7 +527,7 @@ __device__ void snappy_process_symbols(unsnap_state_s *s, int t, Storage &temp_s
     int32_t batch_len, blen_t, dist_t;
 
     if (t == 0) {
-      while ((batch_len = s->q.batch_len[batch]) == 0) { busy_wait(20); }
+      while ((batch_len = s->q.batch_len[batch]) == 0) { busy_wait(100); }
     } else {
       batch_len = 0;
     }
