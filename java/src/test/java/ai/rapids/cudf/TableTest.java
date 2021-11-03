60dc5f42d09716551a591a4ed7f0964b5f5ac49c
Alessandro Bellina <abellina@nvidia.com>
2021-11-02 22:59:01 -0500

    fix offsets

--- java/src/test/java/ai/rapids/cudf/TableTest.java
+++ java/src/test/java/ai/rapids/cudf/TableTest.java
@@ -2274,6 +2274,60 @@ public class TableTest extends CudfTestBase {
   }
 
   @Test
+  void testLeftSemiJoinHashVsAST100PCTLargeLeft() {
+    BinaryOperation expr = new BinaryOperation(BinaryOperator.EQUAL,
+            new ColumnReference(0, TableReference.LEFT),
+            new ColumnReference(0, TableReference.RIGHT));
+    int numKeysLeft = 1000000;
+    int numKeysRight = 100;
+    Integer[] left = new Integer[numKeysLeft];
+    Integer[] right = new Integer[numKeysRight];
+    for (int i = 0; i < numKeysLeft; i++) {
+      left[i] = i;
+    }
+    for (int i = 0; i < numKeysRight; i++) {
+      right[i] = i;
+    }
+    try (Table leftKeys = new Table.TestBuilder().column(left).build();
+         Table rightKeys = new Table.TestBuilder().column(right).build();
+         CompiledExpression condition = expr.compile()) {
+      try(NvtxRange r1 = new NvtxRange("hash_100pct_ll", NvtxColor.BLUE)) {
+        try (GatherMap map = leftKeys.leftSemiJoinGatherMap(rightKeys, false)) {}
+      }
+      try(NvtxRange r1 = new NvtxRange("ast_100pct_ll", NvtxColor.RED)) {
+        try (GatherMap mapAst = leftKeys.conditionalLeftSemiJoinGatherMap(rightKeys, condition)) {}
+      }
+    }
+  }
+
+  @Test
+  void testLeftSemiJoinHashVsAST100PCTLargeRight() {
+    BinaryOperation expr = new BinaryOperation(BinaryOperator.EQUAL,
+            new ColumnReference(0, TableReference.LEFT),
+            new ColumnReference(0, TableReference.RIGHT));
+    int numKeysLeft = 100;
+    int numKeysRight = 1000000;
+    Integer[] left = new Integer[numKeysLeft];
+    Integer[] right = new Integer[numKeysRight];
+    for (int i = 0; i < numKeysLeft; i++) {
+      left[i] = i;
+    }
+    for (int i = 0; i < numKeysRight; i++) {
+      right[i] = i;
+    }
+    try (Table leftKeys = new Table.TestBuilder().column(left).build();
+         Table rightKeys = new Table.TestBuilder().column(right).build();
+         CompiledExpression condition = expr.compile()) {
+      try(NvtxRange r1 = new NvtxRange("hash_100pct_lr", NvtxColor.BLUE)) {
+        try (GatherMap map = leftKeys.leftSemiJoinGatherMap(rightKeys, false)) {}
+      }
+      try(NvtxRange r1 = new NvtxRange("ast_100pct_lr", NvtxColor.RED)) {
+        try (GatherMap mapAst = leftKeys.conditionalLeftSemiJoinGatherMap(rightKeys, condition)) {}
+      }
+    }
+  }
+
+  @Test
   void testLeftSemiJoinHashVsAST75PCT() {
     BinaryOperation expr = new BinaryOperation(BinaryOperator.EQUAL,
             new ColumnReference(0, TableReference.LEFT),
@@ -2290,10 +2344,10 @@ public class TableTest extends CudfTestBase {
     try (Table leftKeys = new Table.TestBuilder().column(left).build();
          Table rightKeys = new Table.TestBuilder().column(right).build();
          CompiledExpression condition = expr.compile()) {
-      try (NvtxRange r1 = new NvtxRange("hash_50pct", NvtxColor.BLUE)) {
+      try (NvtxRange r1 = new NvtxRange("hash_75pct", NvtxColor.BLUE)) {
         try (GatherMap map = leftKeys.leftSemiJoinGatherMap(rightKeys, false)) {}
       }
-      try (NvtxRange r1 = new NvtxRange("ast_50pct", NvtxColor.RED)) {
+      try (NvtxRange r1 = new NvtxRange("ast_75pct", NvtxColor.RED)) {
         try (GatherMap mapAst = leftKeys.conditionalLeftSemiJoinGatherMap(rightKeys, condition)) {}
       }
     }
