c77c7d666e5bef3ce5fe4a3a86f62604d66fd5fa
Alessandro Bellina <abellina@nvidia.com>
2025-07-21 09:07:22 -0700

    fix a few things

--- java/src/main/java/ai/rapids/cudf/Table.java
+++ java/src/main/java/ai/rapids/cudf/Table.java
@@ -176,17 +176,30 @@ public final class Table implements AutoCloseable {
     return new Table(removeNullMasksIfNeeded(table.nativeHandle));
   }
 
+  public static long sortMergeInnerJoinCtr(Table buildTable, boolean buildTableSorted) {
+    return sortMergeInnerJoinCtr(buildTable.nativeHandle, buildTableSorted);
+  }
+
+  public static long sortMergeInnerJoinMakePartitionContext(
+      long joinObj, Table streamTable, boolean streamTableSorted) {
+    return sortMergeInnerJoinMakePartitionContext(joinObj, streamTable.nativeHandle, streamTableSorted);
+  }
+
+  public static long sortMergeInnerJoinMakePartitionedJoin(
+      long joinObj, long partitionCtx, long startRow, long numRows) {
+    return sortMergeInnerJoinPartitionedJoin(joinObj, partitionCtx, startRow, numRows);
+  }
+
   /////////////////////////////////////////////////////////////////////////////
   // NATIVE APIs
   /////////////////////////////////////////////////////////////////////////////
   public static native long sortMergeInnerJoinCtr(
-    long buildTableView, long buildTableSorted);
+    long buildTableView, boolean buildTableSorted);
   public static native long sortMergeInnerJoinMakePartitionContext(
-    long joinObj, long streamTableView, long streamTableSorted);
+    long joinObj, long streamTableView, boolean streamTableSorted);
   public static native long sortMergeInnerJoinPartitionedJoin(
     long joinObj, long partitionCtx, long startRow, long numRows);
-  public static native long[] sortMergeInnerJoinNumRows(
-    long partitionCtx);
+  public static native long[] sortMergeInnerJoinNumRows(long partitionCtx);
 
   private static native long[] removeNullMasksIfNeeded(long tableView) throws CudfException;
 
